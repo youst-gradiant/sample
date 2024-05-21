42cdd33146ef65b2c4f08d8f52ba3c8a76a10458
유성태 <youst@gradiant.team>
2024-05-21 15:51:52 +0900

    ttt

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   rewrites: () => [
     {
       source: "/api/:path*",
-      destination: "http://218.237.253.138:3100/api/:path*",
+      destination: "http://218.237.253.139:3100/api/:path*",
     },
   ],
 };
