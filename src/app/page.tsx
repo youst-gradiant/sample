4a32d3d9d665ec7db538ed91faa2b2a5259fe73d
유성태 <youst@gradiant.team>
2024-05-21 14:01:21 +0900

    fetch추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
+"use client";
+
 import Image from "next/image";
 
 export default function Home() {
+  const fetchUsers = async () => {
+    const response = await fetch("http://218.237.253.138:3100/api/posts");
+
+    console.log(response);
+  };
+
+  fetchUsers();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
