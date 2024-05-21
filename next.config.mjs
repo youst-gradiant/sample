d1c1930f19462275180c2edaac4aeb7e43e661e0
유성태 <youst@gradiant.team>
2024-05-21 14:21:26 +0900

    ddd

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   rewrites: () => [
     {
       source: "/api/posts",
-      destination: "http://218.237.253.138:3000/api/posts",
+      destination: "http://218.237.253.138:3100/api/posts",
     },
   ],
 };
